b921ee2b122b1dd420e68838c10c01413f0695d2
Guillermo Garcia <ggq0107@hotmail.com>
2021-03-22 16:51:18 -0400

    all product routes work

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -29,12 +29,12 @@ router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   try {
-    Category.findByPk(req.params.id,{
+    Product.findByPk(req.params.id,{
         include: [{ 
-          model: Product,
+          model: Category,
          },
          {
-           model: Tag,
+           model: Tag
          }
       ]})
       .then((results => {
@@ -121,6 +121,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  try{Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(productData => {
+      if (!productData) {
+        res.status(404).json({ message: 'No Category found with that ID.' });
+        return;
+      }
+      res.json(productData);
+    })
+  } catch(err) {
+      console.log(err);
+      res.status(500).json(err);
+    };
 });
 
 module.exports = router;
